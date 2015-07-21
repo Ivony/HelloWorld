dffd55603e92768e7eaeb095dd64ab7205741b73
Ivony <Ivony@live.com>
2015-07-21 19:12:08 +0800

    初始化项目和类型

---
+++ HelloWorld/HelloWorld.Core/GameDataHost.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Core
+{
+  public abstract class GameDataHost
+  {
+    public abstract Place GetPlace( Coordinate coordinate );
+
+
+    public abstract Player GetPlayer( Guid userId );
+
+  }
+}
---
+++ HelloWorld/HelloWorld.Core/JsonDataHost.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Core
+{
+  public class JsonDataHost : GameDataHost
+  {
+    public override Place GetPlace( Coordinate coordinate )
+    {
+      
+    }
+
+    public override Player GetPlayer( Guid userId )
+    {
+      throw new NotImplementedException();
+    }
+  }
+}

64e3b77f176ccee274fd57920591cedf97eb40a0
Ivony <Ivony@live.com>
2015-08-30 15:00:46 +0800

    GameRules 改为抽象类，具体实现移动到 HelloWorld.GreateCivilization项目

---
+++ HelloWorld/HelloWorld.Core/GameRules.cs
@@ -0,0 +1,212 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace HelloWorld
+{
+
+
+  /// <summary>
+  /// 游戏全局规则和对象管理器
+  /// </summary>
+  public abstract class GameRules : GameRulesBase
+  {
+
+
+    private GameRuleDataItemCollection _collection;
+
+    private Dictionary<BuildingDescriptor, HashSet<ActionDescriptor>> _actionMap = new Dictionary<BuildingDescriptor, HashSet<ActionDescriptor>>();
+
+
+
+    private IJsonDataResolver _dataResolver;
+    private ITypeResolver _typeResolver;
+
+    protected GameRules( IJsonDataResolver dataResolver, ITypeResolver typeResolver = null )
+    {
+      _dataResolver = dataResolver ?? new JsonDataResolver();
+      _typeResolver = typeResolver ?? new TypeResolver();
+    }
+
+
+
+    /// <summary>
+    /// 初始化游戏规则
+    /// </summary>
+    public override void Initialize()
+    {
+
+      foreach ( var dataItem in LoadRulesData() )
+      {
+
+        var item = LoadData( dataItem );
+        _collection.Add( item );
+
+
+
+
+        var action = item as ActionDescriptor;
+        if ( action != null )
+        {
+          if ( _actionMap.ContainsKey( action.Building ) == false )
+            _actionMap.Add( action.Building, new HashSet<ActionDescriptor>() { action } );
+
+          else
+            _actionMap[action.Building].Add( action );
+          return;
+        }
+      }
+    }
+
+
+
+    /// <summary>
+    /// 加载所有的游戏规则数据
+    /// </summary>
+    /// <returns></returns>
+    protected virtual IEnumerable<JObject> LoadRulesData()
+    {
+      return _dataResolver.LoadAllData();
+    }
+
+
+
+
+    /// <summary>
+    /// 加载游戏数据对象
+    /// </summary>
+    /// <param name="data"></param>
+    /// <returns></returns>
+    protected virtual GameRuleDataItem LoadData( JObject data )
+    {
+      var id = data.GuidValue( "ID" );
+      var type = GetType( data.Value<String>( "Type" ) );
+
+      if ( type == null )
+        return null;
+
+      if ( typeof( GameRuleDataItem ).IsAssignableFrom( type ) == false )
+        return null;
+
+      var method = type.GetMethod( "FromData", BindingFlags.Static | BindingFlags.Public | BindingFlags.FlattenHierarchy );
+      if ( method == null )
+        return null;
+
+      return (GameRuleDataItem) method.Invoke( null, new object[] { id, data } );
+
+    }
+
+
+    /// <summary>
+    /// 根据名称获取类型
+    /// </summary>
+    /// <param name="typeName"></param>
+    /// <returns></returns>
+    protected virtual Type GetType( string typeName )
+    {
+      return _typeResolver.GetType( typeName );
+    }
+
+
+
+
+
+    public override GameRuleDataItem GetDataItem( Guid id )
+    {
+
+      if ( _collection == null )
+        throw new InvalidOperationException( "游戏规则尚未初始化" );
+
+
+      if ( _collection.Contains( id ) == false )
+        throw new InvalidDataException( "未注册的游戏数据 ID: " + id );
+
+
+      return _collection[id];
+    }
+
+
+
+
+    /// <summary>
+    /// 获取某个建筑可以进行的生产
+    /// </summary>
+    /// <param name="place"></param>
+    /// <returns></returns>
+    public override ActionDescriptor[] GetActions( Place place )
+    {
+      if ( _actionMap == null )
+        throw new InvalidOperationException( "游戏规则尚未初始化" );
+
+
+      HashSet<ActionDescriptor> result;
+      if ( _actionMap.TryGetValue( place.Building, out result ) )
+        return result.ToArray();
+
+      else
+        return new ActionDescriptor[0];
+    }
+
+
+
+
+    private BuildingDescriptor _initiationBuilding;
+    /// <summary>
+    /// 初始建筑
+    /// </summary>
+    public override BuildingDescriptor InitiationBuilding { get { return _initiationBuilding; } }
+
+
+
+    /// <summary>
+    /// 获取玩家初始点
+    /// </summary>
+    /// <param name="dataService">数据服务</param>
+    /// <returns></returns>
+    public override Coordinate GetInitiation()
+    {
+
+      var i = 0;
+      while ( i < 100 )
+      {
+
+        i++;
+        var initiation = Coordinate.RandomCoordinate( 1000, 1000 );
+
+        if ( initiation.NearlyCoordinates( 6 ).Any( item => GameHost.DataService.GetPlace( item ).Owner != null ) )
+          continue;
+
+        return initiation;
+      }
+
+
+      throw new InvalidOperationException( "无法找到合适的初始点" );
+
+    }
+
+
+    /// <summary>
+    /// 初始化玩家对象
+    /// </summary>
+    /// <param name="player"></param>
+    public override void InitializePlayer( GamePlayer player )
+    {
+      GameHost.DataService.GetPlace( player.Initiation ).Owner = player;
+
+
+      foreach ( var coordinate in player.Initiation.NearlyCoordinates( 3 ) )
+      {
+        var place = GameHost.DataService.GetPlace( coordinate );
+        place.Owner = player;
+      }
+    }
+
+  }
+}
---
+++ HelloWorld/HelloWorld.Core/GameRulesBase.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+  public abstract class GameRulesBase
+  {
+
+
+    /// <summary>
+    /// 定义一个游戏规则容器，可以按照规则唯一标识获取规则数据
+    /// </summary>
+    protected class GameRuleDataItemCollection : KeyedCollection<Guid, GameRuleDataItem>
+    {
+      protected override Guid GetKeyForItem( GameRuleDataItem item )
+      {
+        return item.Guid;
+      }
+    }
+
+    /// <summary>
+    /// 初始化游戏环境
+    /// </summary>
+    /// <param name="typeResolver"></param>
+    public abstract void Initialize();
+
+
+
+    public abstract GameRuleDataItem GetDataItem( Guid id );
+
+
+
+    public virtual T GetDataItem<T>( Guid id ) where T : GameRuleDataItem
+    {
+
+      var result = GetDataItem( id ) as T;
+      if ( result == null )
+        throw new InvalidDataException( "游戏数据注册类型错误 ID: " + id );
+
+      return result;
+    }
+
+
+
+    /// <summary>
+    /// 获取某个地块可以进行的活动
+    /// </summary>
+    /// <param name="building"></param>
+    /// <returns></returns>
+    public abstract ActionDescriptor[] GetActions( Place place );
+
+
+
+    /// <summary>
+    /// 获取初始建筑
+    /// </summary>
+    public abstract BuildingDescriptor InitiationBuilding { get; }
+
+
+
+    /// <summary>
+    /// 获取玩家初始点
+    /// </summary>
+    /// <param name="dataService">数据服务</param>
+    /// <returns></returns>
+    public abstract Coordinate GetInitiation();
+
+
+    /// <summary>
+    /// 初始化玩家对象
+    /// </summary>
+    /// <param name="player"></param>
+    public abstract void InitializePlayer( GamePlayer player );
+
+
+  }
+}

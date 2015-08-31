dfa5f4795af6611176814fbb04e987bd9d0565ac
Ivony <Ivony@live.com>
2015-08-31 23:54:32 +0800

    优化代码

---
+++ HelloWorld/HelloWorld.Core/Probability.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+
+  /// <summary>
+  /// 提供随机数产生和概率计算的类型
+  /// </summary>
+  public static class Probability
+  {
+
+    private static Random random = new Random();
+
+    private static object sync = new object();
+
+
+    /// <summary>
+    /// 获取一个随机的数值
+    /// </summary>
+    /// <param name="maxValue">最大值</param>
+    /// <returns></returns>
+    public static int RandomNumber( int maxValue )
+    {
+      lock ( sync )
+        return random.Next( maxValue );
+    }
+
+
+    /// <summary>
+    /// 如果以指定概率计算，确定是否命中
+    /// </summary>
+    /// <param name="hitProbability">命中概率</param>
+    /// <returns>是否命中</returns>
+    public static bool HasHit( double hitProbability )
+    {
+      lock ( sync )
+        return random.NextDouble() < hitProbability;
+    }
+
+
+    /// <summary>
+    /// 如果以指定的概率计算，若命中，则执行指定方法
+    /// </summary>
+    /// <param name="hitProbability">命中概率</param>
+    /// <param name="action">命中后要执行的方法</param>
+    /// <returns>是否命中</returns>
+    public static bool IfHit( double hitProbability, Action action )
+    {
+      if ( HasHit( hitProbability ) )
+      {
+        action();
+        return true;
+      }
+
+      else
+        return false;
+
+    }
+
+  }
+}
--- HelloWorld/HelloWorld.GreatCivilization/Wilderness.cs
+++ HelloWorld/HelloWorld.GreatCivilization/Wilderness.cs
@@ -15,28 +15,23 @@ namespace HelloWorld.GreatCivilization
 
 
 
+    private static readonly Guid forestId = new Guid( "8105A10F-1E11-4FF0-B2D2-1FD910258648" );
+    private static readonly Guid grasslandId = new Guid( "C6E4329A-D722-439C-B846-F0EADF388AD9" );
+
 
     public override void Check( Place place )
     {
 
-      for ( int i = 0; i < (DateTime.UtcNow - place.CheckPoint).Minutes; i++ )
+      for ( int i = 0; i < ( DateTime.UtcNow - place.CheckPoint ).Minutes; i++ )
       {
-        var r = GameHost.Random( 100 );
-        if ( r < 1 )
-        {
-          place.Building = GameHost.GameRules.GetDataItem<BuildingDescriptor>( new Guid( "8105A10F-1E11-4FF0-B2D2-1FD910258648" ) );
+        if ( Probability.IfHit( 1 / 100d, () => GameHost.GameRules.GetDataItem<BuildingDescriptor>( forestId ) ) )
           break;
-        }
-        else if ( r < 3 )
-        {
-          place.Building = GameHost.GameRules.GetDataItem<BuildingDescriptor>( new Guid( "C6E4329A-D722-439C-B846-F0EADF388AD9" ) );
+
+        if ( Probability.IfHit( 1 / 100d, () => GameHost.GameRules.GetDataItem<BuildingDescriptor>( grasslandId ) ) )
           break;
-        }
-
-
       }
 
-
+      base.Check( place );
     }
 
 
